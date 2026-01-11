bae8b92ecee24fac0398cd387e6310b30d147fd1
Oumar Bajabour <bajabouroumar@gmail.com>
2026-01-11 19:22:39 +0100

    changes

--- src/firebase.ts
+++ src/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCiCkqrHcBJZVcy6RXsBKeb-NZXnKsAD6s",
   authDomain: "gratis-ngo-7bb44.firebaseapp.com",
   projectId: "gratis-ngo-7bb44",
-  storageBucket: "gratis-ngo-7bb44.appspot.com",
+  storageBucket: "gratis-ngo-7bb44.firebasestorage.app",
   messagingSenderId: "659832345710",
   appId: "1:659832345710:web:8048362b3636c465f4c27f",
   measurementId: "G-LF6HJ0RP8D"
